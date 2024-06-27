17ce6b8c29aa1d63231c39518b50c033bb2c4a29
Laurina23 <laurinapatnaik20@gmail.com>
2024-06-27 14:22:44 +0530

    Update prog2.kt

--- prog2.kt
+++ prog2.kt
@@ -2,7 +2,7 @@ fun main() {
     val n = 5
     val totStars = 9
     for (i in 0 until n) {
-        val stars = 1 + 2 * i
+        val stars = 2 * i + 1
         val spaces = (totStars - stars) / 2
 
         for (j in 0 until spaces) {
@@ -21,7 +21,7 @@ fun main() {
     println()
 
     for (i in (n - 2) downTo 0) {
-        val stars = 1 + 2 * i
+        val stars = 2 * i + 1
         val spaces = (totStars - stars) / 2
         for (j in 0 until spaces) {
             print(" ")
