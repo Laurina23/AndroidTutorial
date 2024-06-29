a2b1d52f30e4b6a801459cbcb58c1a422762177e
Laurina23 <laurinapatnaik20@gmail.com>
2024-06-29 16:08:28 +0530

    Deleted directory

--- day2/fun1.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val a = 17
-    val b = 43
-    println("The larger number is: ${largest(a, b)}")
-}
-fun largest(a: Int, b: Int): Int {
-    return if (a > b) a else b
-}
-
--- day2/fun2.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    val numbers = listOf(10,11,12,13,14)
-    println("The sum of the list is: ${sumOfList(numbers)}")
-}
-fun sumOfList(numbers: List<Int>): Int {
-    return numbers.sum()
-}
-
